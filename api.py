258be1cf5946c0ffcdfa0ab1525cf99dd1d80a62
David <david_soc@hotmail.com>
2019-11-12 21:18:59 -0500

    methode lister et debuter

--- api.py
+++ api.py
@@ -1,1 +1,35 @@
-print('alklojajo')
+import requests
+
+
+def lister_parties(idul):
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+
+    rep = requests.get(url_base+'lister/', params={'idul': idul})
+    if rep.status_code == 200:
+        # la requête s'est déroulée normalement; décoder le JSON
+        rep = rep.json()
+        if 'message' in rep:
+            raise RuntimeError
+        print(rep)
+        return rep
+    
+    else:
+        print(f"Le GET sur {url_base+'lister'} a produit le code d'erreur {rep.status_code}.")
+
+def débuter_partie(idul):
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+    rep = requests.post(url_base+'débuter/', data={'idul': idul})
+    if rep.status_code == 200:
+        # la requête s'est déroulée normalement; décoder le JSON
+        rep = rep.json()
+        if 'message' in rep:
+            raise RuntimeError
+        print(rep)
+        return (rep['id'], rep['état'])
+    
+    else:
+        print(f"Le POST sur {url_base+'debuter'} a produit le code d'erreur {rep.status_code}.")
+
+
+
+lister_parties('daiba')
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('google chrome,com')
-import api
+import argparse
+
+
+def analyser_commande():
+    # créer un analyseur de ligne de commande
+    parser = argparse.ArgumentParser(description = "Jeux Quoridor - phase 1")
+    
+    # insérer ici avec les bons appels à la méthode add_argument
+    parser.add_argument('idul', help = 'IDUL du joueur.')
+    parser.add_argument('-l', '--lister', help = 'Lister les identifiants de vos 20 dernières parties', action = 'store_true')
+    return parser.parse_args()
+analyser_commande()
